c2b7085a92622ac942b086839687bea856c7b7e0
Timothy-Achonu <achonutimothy@gmail.com>
2022-06-13 22:44:07 +0100

    committing changes

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const ratingNumbers = document.querySelector('.rating-numbers');
 ratingNumbers.addEventListener('click', styleButtons)
-console.log(ratingNumbers)
+// console.log(ratingNumbers.children)
 
 let checkSelectedRating = false;
-let selectedButton;
+let selectedRating;
+const eachRatingNumber = document.querySelectorAll('.select-button')
+// console.log(eachRatingNumber)
+let count = 0;
+let lastClickedButton;
 function styleButtons(e) {
+    
     if(e.target.matches('.select-button')) {
-        e.target.style.backgroundColor = 'hsl(217, 12%, 54%)';
-        e.target.style.color = 'white';
-        selectedButton =  e.target.innerText ;
+        count++;
+        // console.log(e.target)
+        e.target.classList.toggle('clicked-button');
+        if(count >= 2) {
+            if(e.target !== lastClickedButton) {
+                // console.log(lastClickedButton);
+                lastClickedButton.removeAttribute('class');
+                lastClickedButton.setAttribute('class', 'select-button')
+                // console.log(lastClickedButton.classList)
+                /*
+                How does removeAttribute method work?
+                */
+            } 
+        }
+        lastClickedButton = e.target;
+        // console.log(lastClickedButton)
+        selectedRating =  e.target.innerText ;
         checkSelectedRating = true;
+        /*
+          I don't get this. This works but, if I keep clicking and
+          unclicking the ratings tryihg to test the code, suddenly 
+          the function stops running when I click the buttons, 
+          only one of them continues to run the function when I 
+          click on it- I know this because it still toggles the
+          'clicked-button' class and things are still printed on
+          the console when that button is clicked.
+        */
     }
 }
 
@@ -28,7 +56,7 @@ function showMessage() {
         setAttribute('class', 'thank-you-message show-message');
 
         document.querySelector('.selected-button').
-        innerText = ` ${selectedButton} `;
+        innerText = ` ${selectedRating} `;
         document.querySelector('.selected-button').style.
         margin = '0px 3px'
     }
